b9e4f3ba6ca8de1dcd2b1784d5f0b799c44e43f2
Geunyoung Kim <emsud@inu.ac.kr>
2019-07-03 00:15:27 +0900

    Window Server

--- ss.py
+++
@@ -1,9 +0,0 @@
-import socket
- 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('192.168.0.26', 5003))
-server_socket.listen(0)
-client_socket, addr = server_socket.accept()
-data = client_socket.recv(65535)
-print(data)
-
